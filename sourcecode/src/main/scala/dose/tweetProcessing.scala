0354ed28b0a7394c5731dd24890bbdd7a60a82ee
Brian Cohn <brian_cohn14@pitzer.edu>
2015-08-05 15:54:45 -0400

    added assembler and clarified filenames See #1

--- sourcecode/src/main/scala/dose/tweetProcessing.scala
+++ sourcecode/src/main/scala/dose/tweetProcessing.scala
@@ -7,8 +7,9 @@ import org.apache.spark.mllib._
 import org.apache.spark.rdd.RDD
 object tweetProcessing {
         def setupSparkProject(args: Array[String]) {
-                val conf = new SparkConf().setAppName("REtweets")
-                val sc = new SparkContext(conf)
-                (conf, sc)
+                // val conf = new SparkConf().setAppName("REtweets")
+                // val sc = new SparkContext(conf)
+                // (conf, sc)
+                println(args)
         }
 }
