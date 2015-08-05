70faacb25db94f25ab6ed5f9373e4d5c296c3036
Brian Cohn <brian_cohn14@pitzer.edu>
2015-08-05 15:16:05 -0400

    directory renaming and movement for package testing See #1

---
+++ sourcecode/src/main/scala/dose/package.scala
@@ -0,0 +1,15 @@
+package dose
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import org.apache.spark.mllib._
+import org.apache.spark.rdd.RDD
+import dose.tool._
+
+object Main {
+  def main(args: Array[String]) = {
+    println("calling")
+    val sparkConfigs = tweetProcessing.setupSparkProject(args)
+  }
+}
--- sourcecode/src/main/scala/dose/tweetProcessing.scala
+++ sourcecode/src/main/scala/dose/tweetProcessing.scala
@@ -1,12 +1,11 @@
+package dose.tool
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
 import org.apache.spark.mllib._
 import org.apache.spark.rdd.RDD
-import bbdl.space._
-package dose.tool
-object project {
+object tweetProcessing {
         def setupSparkProject(args: Array[String]) {
                 val conf = new SparkConf().setAppName("REtweets")
                 val sc = new SparkContext(conf)
